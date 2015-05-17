fa8e5a617982ee092a4af6b807ef6e4c130178ef
Lukas van Driel <lukas@q42.nl>
2015-05-17 15:27:30 +0200

    added analytics & spiderable for scalingo

--- moorddiner.js
+++ moorddiner.js
@@ -5,13 +5,16 @@ Router.configure({
 });
 
 Router.route('/', function () {
+  GAnalytics.pageview();
   this.render('home');
 });
 
 Router.route('/:_template', function () {
+  GAnalytics.pageview();
   this.render(this.params._template);
 });
 
 Router.route('/moorddiners/:_diner', function () {
+  GAnalytics.pageview();
   this.render(this.params._diner);
 });
