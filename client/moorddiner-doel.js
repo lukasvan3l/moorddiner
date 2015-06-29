f70257f2cbdb42ddabf93fc431679a7b9ea343e9
Lukas van Driel <lukas@q42.nl>
2015-06-29 23:44:45 +0200

    nogiets

--- client/moorddiner-doel.js
+++ client/moorddiner-doel.js
@@ -4,6 +4,7 @@ Template.doel.events({
     Meteor.call('sendDemoMail', $('#mce-EMAIL').val(), $('#mce-FNAME').val(), function(error, result) {
       if (error) console.error(error);
       console.log(result);
+      $('#mc-embedded-subscribe-form').html('<h2>Verstuurd</h2>Dank voor je interesse. De email is succesvol verstuurd.');
     });
     return false;
   }
